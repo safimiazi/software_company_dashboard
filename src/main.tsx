4e9c0316265b83bfb14b0096062abce77a97bbf5
safi miazi <mohibullamiazi@gmail.com>
2025-01-16 22:41:03 +0600

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRoutes from "./routes/AppRoutes.tsx";
+import { ConfigProvider } from "antd";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <AppRoutes/>
+    <ConfigProvider theme={{ token: { colorPrimary: '#FF3C00' } }}>
+      <AppRoutes />
+    </ConfigProvider>
   </StrictMode>
 );
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,19 @@
+import { Button } from "antd";
+import Container from "../../components/common/Container/Container";
 
 const Login = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="py-20">
+      <Container>
+        <div className="flex flex-col justify-center items-center">
+          <div className="border  w-1/3 h-56 rounded-md">
+          
+          <Button type="primary">Button</Button>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Login;
