32234cba5324d4e7d814c8935a12e0b78c81fa58
safi miazi <mohibullamiazi@gmail.com>
2025-01-17 13:55:07 +0600

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRoutes from "./routes/AppRoutes.tsx";
 import { ConfigProvider } from "antd";
+import { Provider } from "react-redux";
+import { store } from "./redux/Store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ConfigProvider theme={{ token: { colorPrimary: '#FF3C00' } }}>
-      <AppRoutes />
-    </ConfigProvider>
+    <Provider store={store}>
+      <ConfigProvider theme={{ token: { colorPrimary: "#FF3C00" } }}>
+        <AppRoutes />
+      </ConfigProvider>
+    </Provider>
   </StrictMode>
 );
--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 const isAuthenticated = () => {
-  return localStorage.getItem("authToken") ? true : false;
+  return localStorage.getItem("authToken") ? false : true;
 };
 
 const ProtectedRoute = ({ children }: any) => {
