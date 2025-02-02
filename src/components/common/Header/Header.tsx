5ae55b6a153b85d244609f9279a7a606368eee85
safi miazi <mohibullamiazi@gmail.com>
2025-02-02 23:58:38 +0600

    Fetch and display user data in Header component

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -2,10 +2,12 @@ import { useDispatch } from "react-redux";
 import { sidebarToggle } from "../../../redux/slices/sidebarSlice";
 import { FaBars } from "react-icons/fa";
 import { MdNotifications } from "react-icons/md";
+import { useGetAdminDataQuery } from "../../../redux/api/adminApi/authApi/AuthApi.query";
 
 const Header = () => {
   const dispatch = useDispatch();
-
+ const {data}= useGetAdminDataQuery({})
+console.log(data)
   return (
     <header className="h-20 w-full flex items-center justify-between bg-white text-gray-800 px-4 lg:px-6 shadow-md">
       {/* Logo Section */}
@@ -28,13 +30,13 @@ const Header = () => {
         {/* User Profile */}
         <div className="flex items-center gap-2">
           <img
-            src="https://via.placeholder.com/40"
+            src={"/user.png"}
             alt="User Avatar"
             className="w-10 h-10 rounded-full object-cover border border-gray-300"
           />
           <div className="hidden lg:block">
-            <p className="text-sm font-medium text-gray-800">John Doe</p>
-            <p className="text-xs text-gray-500">Admin</p>
+            <p className="text-sm font-medium text-gray-800">{data?.data?.name}</p>
+            <p className="text-xs text-gray-500">{data?.data?.role}</p>
           </div>
         </div>
       </div>
